9d2361006202149e24146af9aaf8afa7aa116fc2
Craig Duff <craig.duff7@mail.dcu.ie>
2017-07-24 20:42:20 -0700

    creates placeholder entries for each store

--- scripts/setnames.py
+++ scripts/setnames.py
@@ -3,17 +3,28 @@ from bs4 import BeautifulSoup
 
 def get_db():
     from pymongo import MongoClient
-    client = MongoClient('mongodb://fftcgscript:nextat765@ds161022.mlab.com:61022/fftcg')
+	#client = MongoClient('mongodb://fftcgscript:nextat765@ds161022.mlab.com:61022/fftcg')
+    client = MongoClient('localhost:27017')
     db = client.fftcg
     return db
 
 def add_card(db, cardname, cardnum):
-    db.cards.update({"cardnum" : cardnum}, {"$set": {"cardname" : cardname, "cardnum" : cardnum} }, upsert=True)
+    db.cards.update(
+    	{"cardnum" : cardnum},
+	    	{"$set": 
+	    		{"cardname" : cardname, 
+	    		"cardnum" : cardnum, 
+	    		"stores" : [
+	    			{ "_id" : "trollandtoad.com" },
+	    			{ "_id" : "fftcgsingles.co.uk" }
+	    		]
+	    	} 
+	    }, upsert=True)
 
 db = get_db()
 #response = requests.get("http://final-fantasy-card-game.wikia.com/wiki/Opus_I_Collection")
-response = requests.get("http://final-fantasy-card-game.wikia.com/wiki/Opus_II_Collection")
-#response = requests.get("http://final-fantasy-card-game.wikia.com/wiki/Opus_III_Collection")
+#response = requests.get("http://final-fantasy-card-game.wikia.com/wiki/Opus_II_Collection")
+response = requests.get("http://final-fantasy-card-game.wikia.com/wiki/Opus_III_Collection")
 
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
