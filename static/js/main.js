c65b1d9618fdfa2c920c6c3ac06cdabec31a4ca4
xubuntu <michael.kerrigan20@gmail.com>
2017-07-25 14:32:48 -0700

    added foils to card table

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,15 @@
-function search() {
-  // Declare variables 
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("table");
-  tr = table.getElementsByTagName("tr");
+$(document).ready(function () {
+    //your code here
+  var $rows = $('#table tr').not(':first');
+  $('#search').keyup(function() {
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
+      var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+          reg = RegExp(val, 'i'),
+          text;
+
+      $rows.show().filter(function() {
+          text = $(this).text().replace(/\s+/g, ' ');
+          return !reg.test(text);
+      }).hide();
+  });
+});
